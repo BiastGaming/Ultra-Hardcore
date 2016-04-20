c220432d8ac2d7ede6f4f177143a9d4b8608bdf3
LeonTG77 <leontg77@gmail.com>
2016-04-20 07:58:18 +0200

    Added team colors

--- src/com/leontg77/ultrahardcore/feature/pvp/LongshotFeature.java
+++ src/com/leontg77/ultrahardcore/feature/pvp/LongshotFeature.java
@@ -1,15 +1,18 @@
 package com.leontg77.ultrahardcore.feature.pvp;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.scoreboard.Team;
 
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.feature.Feature;
+import com.leontg77.ultrahardcore.managers.TeamManager;
 import com.leontg77.ultrahardcore.scenario.ScenarioManager;
 import com.leontg77.ultrahardcore.scenario.scenarios.RewardingLongshots;
 import com.leontg77.ultrahardcore.scenario.scenarios.RewardingLongshotsPlus;
@@ -22,14 +25,18 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class LongshotFeature extends Feature implements Listener {
-	private final ScenarioManager scen;
 	private final Game game;
+	
+	private final ScenarioManager scen;
+	private final TeamManager teams;
 
-	public LongshotFeature(Game game, ScenarioManager scen) {
+	public LongshotFeature(Game game, ScenarioManager scen, TeamManager teams) {
 		super("Longshot", "Displays in the chat if someone gets a long shot of 50+ blocks.");
 		
-		this.scen = scen;
 		this.game = game;
+		
+		this.teams = teams;
+		this.scen = scen;
 	}
 	
 	@EventHandler(ignoreCancelled = true)
@@ -59,6 +66,22 @@ public class LongshotFeature extends Feature implements Listener {
 			return;
 		}
 		
-		PlayerUtils.broadcast(Main.PREFIX + "§6" + killer.getName() + " §7got a longshot of §a" + NumberUtils.formatDouble(distance) + " §7blocks on §6" + player.getName() + "§7.");
+		PlayerUtils.broadcast(Main.PREFIX + name(killer) + " §7got a longshot of §a" + NumberUtils.formatDouble(distance) + " §7blocks on §6" + name(player) + "§7.");
+	}
+	
+	/**
+	 * Get the name of the given player with the team color if any.
+	 * 
+	 * @param player The player to use.
+	 * @return The name with the team color.
+	 */
+	private String name(Player player) {
+		Team team = teams.getTeam(player);
+		
+		if (team == null) {
+			return ChatColor.WHITE + player.getName();
+		} else {
+			return team.getPrefix() + player.getName();
+		}
 	}
 }
--- src/com/leontg77/ultrahardcore/feature/pvp/ShootHealthFeature.java
+++ src/com/leontg77/ultrahardcore/feature/pvp/ShootHealthFeature.java
@@ -1,5 +1,6 @@
 package com.leontg77.ultrahardcore.feature.pvp;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -7,12 +8,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Team;
 
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.feature.Feature;
+import com.leontg77.ultrahardcore.managers.TeamManager;
 import com.leontg77.ultrahardcore.scenario.ScenarioManager;
 import com.leontg77.ultrahardcore.scenario.scenarios.Paranoia;
+import com.leontg77.ultrahardcore.scenario.scenarios.SelfDiagnosis;
 import com.leontg77.ultrahardcore.scenario.scenarios.TeamHealth;
 import com.leontg77.ultrahardcore.utils.NumberUtils;
 
@@ -23,17 +27,19 @@ import com.leontg77.ultrahardcore.utils.NumberUtils;
  */
 public class ShootHealthFeature extends Feature implements Listener {
 	private final Main plugin;
+	private final Game game;
 	
 	private final ScenarioManager scen;
-	private final Game game;
+	private final TeamManager teams;
 
-	public ShootHealthFeature(Main plugin, Game game, ScenarioManager scen) {
+	public ShootHealthFeature(Main plugin, Game game, ScenarioManager scen, TeamManager teams) {
 		super("Shoot Health", "Displays the shot persons health to the shooter.");
 
 		this.plugin = plugin;
-		
-		this.scen = scen;
 		this.game = game;
+
+		this.teams = teams;
+		this.scen = scen;
 	}
 	
 	@EventHandler(ignoreCancelled = true)
@@ -41,7 +47,7 @@ public class ShootHealthFeature extends Feature implements Listener {
 		Entity attacked = event.getEntity();
 		Entity attacker = event.getDamager();
 		
-    	if (game.isRecordedRound() || scen.getScenario(TeamHealth.class).isEnabled() || scen.getScenario(Paranoia.class).isEnabled()) {
+    	if (game.isRecordedRound() || scen.getScenario(TeamHealth.class).isEnabled() || scen.getScenario(SelfDiagnosis.class).isEnabled() || scen.getScenario(Paranoia.class).isEnabled()) {
 			return;
 		}
     	
@@ -64,9 +70,25 @@ public class ShootHealthFeature extends Feature implements Listener {
 				String percent = NumberUtils.makePercent(health);
 				
 				if (health > 0.0000) {
-					killer.sendMessage(Main.PREFIX + "§6" + player.getName() + " §7is now at §a" + percent + "%");
+					killer.sendMessage(Main.PREFIX + name(player)+ " §7is now at §a" + percent + "%");
 				}
 			}
 		}.runTaskLater(plugin, 1);
+	}
+	
+	/**
+	 * Get the name of the given player with the team color if any.
+	 * 
+	 * @param player The player to use.
+	 * @return The name with the team color.
+	 */
+	private String name(Player player) {
+		Team team = teams.getTeam(player);
+		
+		if (team == null) {
+			return ChatColor.WHITE + player.getName();
+		} else {
+			return team.getPrefix() + player.getName();
+		}
 	}
 }
