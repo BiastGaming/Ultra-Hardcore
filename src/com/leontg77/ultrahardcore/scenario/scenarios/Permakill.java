fcf520e2851aedcddd0480d4ebbc3208eea9a2c0
LeonTG77 <leontg77@gmail.com>
2016-04-20 07:56:16 +0200

    Added a recovery method

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Permakill.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Permakill.java
@@ -6,17 +6,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import com.leontg77.ultrahardcore.Game;
+import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.events.GameStartEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 
 /**
- * Permakill scenario class
+ * Permakill scenario class.
  * 
  * @author LeonTG77
  */
 public class Permakill extends Scenario implements Listener {
 	private final Game game;
 
+	/**
+	 * Permakill class constructor.
+	 * 
+	 * @param game The game class.
+	 */
 	public Permakill(Game game) {
 		super("Permakill", "Everytime a player dies it toggles between perma day and perma night");
 		
@@ -28,11 +34,26 @@ public class Permakill extends Scenario implements Listener {
 		game.getWorld().setGameRuleValue("doDaylightCycle", "true");
 	}
 	
+	@Override
+	public void onEnable() {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		on(new GameStartEvent());
+	}
+	
+	@EventHandler
+	public void on(GameStartEvent event) {
+		game.getWorld().setGameRuleValue("doDaylightCycle", "false");
+		game.getWorld().setTime(6000);
+	}
+	
 	@EventHandler
 	public void on(PlayerDeathEvent event) {
 		Player player = event.getEntity();
 		
-		if (!game.getPlayers().contains(player)) {
+		if (!game.getWorlds().contains(player.getWorld())) {
 			return;
 		}
 		
@@ -41,11 +62,5 @@ public class Permakill extends Scenario implements Listener {
 		} else {
 			game.getWorld().setTime(6000);
 		}
-	}
-	
-	@EventHandler
-	public void on(GameStartEvent event) {
-		game.getWorld().setGameRuleValue("doDaylightCycle", "false");
-		game.getWorld().setTime(6000);
 	}
 }
