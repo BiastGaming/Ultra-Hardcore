0c7d1d3411f3e8b4c9d0dc5e87f63551951cc65d
LeonTG77 <leontg77@gmail.com>
2016-04-20 07:52:59 +0200

    Added the Fireless scenario

---
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Fireless.java
@@ -0,0 +1,45 @@
+package com.leontg77.ultrahardcore.scenario.scenarios;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import com.leontg77.ultrahardcore.State;
+import com.leontg77.ultrahardcore.scenario.Scenario;
+
+/**
+ * Fireless scenario class.
+ * 
+ * @author LeonTG77
+ */
+public class Fireless extends Scenario implements Listener {
+
+	/**
+	 * Fireless class constructor.
+	 */
+	public Fireless() {
+		super("Fireless", "You cannot take fire or lava damage.");
+	}
+
+    @EventHandler(ignoreCancelled = true)
+    public void on(EntityDamageEvent event) {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		if (!(event.getEntity() instanceof Player)) {
+			return;
+		}
+        
+		switch (event.getCause()) {
+		case FIRE:
+		case FIRE_TICK:
+		case LAVA:
+			event.setCancelled(true);
+			break;
+		default:
+			break;
+		}
+    }
+}
