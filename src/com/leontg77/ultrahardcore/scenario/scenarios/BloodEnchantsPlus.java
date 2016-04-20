d2ade65ce22661262622b027ae56b2edeee9e761
LeonTG77 <leontg77@gmail.com>
2016-04-20 07:50:38 +0200

    Added the BloodEnchants+ scenario

---
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/BloodEnchantsPlus.java
@@ -0,0 +1,46 @@
+package com.leontg77.ultrahardcore.scenario.scenarios;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+
+import com.leontg77.ultrahardcore.State;
+import com.leontg77.ultrahardcore.events.AnvilRepairEvent;
+import com.leontg77.ultrahardcore.scenario.Scenario;
+import com.leontg77.ultrahardcore.utils.PlayerUtils;
+
+/**
+ * BloodEnchants+ scenario class.
+ * 
+ * @author LeonTG77
+ */
+public class BloodEnchantsPlus extends Scenario implements Listener {
+	
+	/**
+	 * BloodEnchants+ class constructor.
+	 */
+	public BloodEnchantsPlus() {
+		super("BloodEnchants+", "Every level of enchanting you use you lose 1/2 a heart for. e.g. if you spend 5 levels of EXP on enchantments you lose 2.5 hearts, This includes Anvils as well.");
+	}
+
+	@EventHandler
+	public void on(EnchantItemEvent event) {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		Player player = event.getEnchanter();
+		PlayerUtils.damage(player, event.getExpLevelCost());
+	}
+	
+	@EventHandler
+	public void on(AnvilRepairEvent event) {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
+		PlayerUtils.damage(player, event.getRepairCost());
+	}
+}
