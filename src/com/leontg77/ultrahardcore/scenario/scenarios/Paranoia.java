8dea99f5aa4531a5e410342afe9e7738270aee02
LeonTG77 <leontg77@gmail.com>
2016-04-23 01:57:41 +0200

    Moved the ore part to spec info

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Paranoia.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Paranoia.java
@@ -1,20 +1,17 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Scoreboard;
 
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.State;
@@ -23,16 +20,22 @@ import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
- * Paranoia scenario class
+ * Paranoia scenario class.
  * 
  * @author LeonTG77
  */
 public class Paranoia extends Scenario implements Listener {
-	public static final String PREFIX = "§cParanoia §8» §f";
+	public static final String PREFIX = "§cParanoia §8» §a";
 	
 	private final BoardManager board;
 	private final Game game;
 	
+	/**
+	 * Paranoia class constructor.
+	 * 
+	 * @param game The game class.
+	 * @param board The board manager class.
+	 */
 	public Paranoia(Game game, BoardManager board) {
 		super("Paranoia", "Your coordinates are broadcasted when you mine diamonds/gold, craft or eat an golden apple, you craft an anvil or enchantment table or you die");
 		
@@ -47,30 +50,8 @@ public class Paranoia extends Scenario implements Listener {
 
 	@Override
 	public void onEnable() {
-		Scoreboard board = this.board.getBoard();
-		
-		board.clearSlot(DisplaySlot.PLAYER_LIST);
-		board.clearSlot(DisplaySlot.BELOW_NAME);
-	}
-	
-	@EventHandler
-	public void on(BlockBreakEvent event) {
-		if (!State.isState(State.INGAME)) {
-			return;
-		}
-		
-		Player player = event.getPlayer();
-		Block block = event.getBlock();
-		
-		Location loc = player.getLocation();
-		
-		if (block.getType() == Material.DIAMOND_ORE) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 mined §bdiamond ore §7at " + location(loc));
-		}
-		
-		if (block.getType() == Material.GOLD_ORE) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 mined §6gold ore §7at " + location(loc));
-		}
+		board.getBoard().clearSlot(DisplaySlot.PLAYER_LIST);
+		board.getBoard().clearSlot(DisplaySlot.BELOW_NAME);
 	}
 	
 	@EventHandler
@@ -80,10 +61,15 @@ public class Paranoia extends Scenario implements Listener {
 		}
 		
 		Player player = event.getPlayer();
+		
+		if (!game.getPlayers().contains(player)) {
+			return;
+		}
+		
 		Location loc = player.getLocation();
 		
 		if (event.getItem().getType() == Material.GOLDEN_APPLE) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 ate a §eGolden Apple §7at " + location(loc));
+			PlayerUtils.broadcast(PREFIX + player.getName() + "§f ate a §eGolden Apple §fat " + location(loc));
 		}
 	}
 	
@@ -96,16 +82,22 @@ public class Paranoia extends Scenario implements Listener {
 		HumanEntity player = event.getWhoClicked();
 		Location loc = player.getLocation();
 		
-		if (event.getRecipe().getResult().getType() == Material.GOLDEN_APPLE) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 crafted a §eGolden Apple §7at " + location(loc));
+		if (!game.getPlayers().contains(player)) {
+			return;
 		}
 		
-		if (event.getRecipe().getResult().getType() == Material.ANVIL) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 crafted an §dAnvil §7at " + location(loc));
+		ItemStack result = event.getRecipe().getResult();
+		
+		if (result.getType() == Material.GOLDEN_APPLE) {
+			PlayerUtils.broadcast(PREFIX + player.getName() + "§f crafted a §eGolden Apple §fat " + location(loc));
 		}
 		
-		if (event.getRecipe().getResult().getType() == Material.ENCHANTMENT_TABLE) {
-			PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 crafted an §5Enchantment Table §7at " + location(loc));
+		if (result.getType() == Material.ANVIL) {
+			PlayerUtils.broadcast(PREFIX + player.getName() + "§f crafted an §dAnvil §fat " + location(loc));
+		}
+		
+		if (result.getType() == Material.ENCHANTMENT_TABLE) {
+			PlayerUtils.broadcast(PREFIX + player.getName() + "§f crafted an §5Enchantment Table §fat " + location(loc) + ".");
 		}
 	}
 	
@@ -117,8 +109,12 @@ public class Paranoia extends Scenario implements Listener {
 		
 		Player player = event.getEntity();
 		Location loc = player.getLocation();
-
-		PlayerUtils.broadcast(PREFIX + ChatColor.GREEN + player.getName() + "§7 died at " + location(loc));
+		
+		if (!game.getWorlds().contains(player.getWorld())) {
+			return;
+		}
+		
+		PlayerUtils.broadcast(PREFIX + player.getName() + "§f died at " + location(loc));
 	}
 
 	/**
@@ -127,7 +123,7 @@ public class Paranoia extends Scenario implements Listener {
 	 * @param loc the given location.
 	 * @return Location in String form.
 	 */
-	private String location(Location loc) {
-		return "x:" + loc.getBlockX() + " y:" + loc.getBlockY() + " z:" + loc.getBlockZ();
+	public String location(Location loc) {
+		return "X: " + loc.getBlockX() + " Y: " + loc.getBlockY() + " Z: " + loc.getBlockZ();
 	}
 }
