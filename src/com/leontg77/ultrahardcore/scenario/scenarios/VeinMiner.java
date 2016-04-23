28864271e759a449879e3856d61454afdd815fd1
LeonTG77 <leontg77@gmail.com>
2016-04-23 16:27:16 +0200

    Updated the description

--- src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
@@ -3,19 +3,13 @@ package com.leontg77.ultrahardcore.scenario.scenarios;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 
 /**
- * VeinMiner scenario class
+ * VeinMiner scenario class.
  * 
  * @author LeonTG77
  */
 public class VeinMiner extends Scenario {
 	
 	public VeinMiner() {
-		super("VeinMiner", "When you mine a vein of an ore the entire thing mines");
+		super("VeinMiner", "When you mine a part of a ore vein while shifting the entire thing mines");
 	}
-
-	@Override
-	public void onDisable() {}
-
-	@Override
-	public void onEnable() {}
 }
