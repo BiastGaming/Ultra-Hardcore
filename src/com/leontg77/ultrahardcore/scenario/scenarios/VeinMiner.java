115f12a23bd03c47968e483a7c9999281e97eff8
LeonTG77 <leontg77@gmail.com>
2016-05-07 02:02:24 +0200

    Finished the scenario code

--- src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
@@ -1,15 +1,71 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.scenario.Scenario;
+import com.leontg77.ultrahardcore.utils.BlockUtils;
 
 /**
  * VeinMiner scenario class.
  * 
  * @author LeonTG77
  */
-public class VeinMiner extends Scenario {
+public class VeinMiner extends Scenario implements Listener {
+	private final Main plugin;
 	
-	public VeinMiner() {
-		super("VeinMiner", "When you mine a part of a ore vein while shifting the entire thing mines");
+	public VeinMiner(Main plugin) {
+		super("VeinMiner", "When you mine a part of a ore vein while shifting the entire thing mines.");
+		
+		this.plugin = plugin;
+	}
+	
+	private static final Predicate<Material> IS_ORE = (m) -> m.name().endsWith("_ORE");
+	
+	@EventHandler
+	public void on(BlockBreakEvent event) {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
+		Block block = event.getBlock();
+		
+		if (!player.isSneaking()) {
+			return;
+		}
+		
+		if (!IS_ORE.test(block.getType())) {
+            return;
+        }
+		
+		List<Block> vein = new ArrayList<Block>();
+		BlockUtils.getVein(block, vein);
+		vein.remove(block);
+		
+		new BukkitRunnable() {
+			public void run() {
+				if (vein.isEmpty()) {
+					cancel();
+					return;
+				}
+				
+				Block block = vein.remove(0);
+				
+				BlockUtils.blockBreak(player, block);
+				block.breakNaturally(player.getItemInHand());
+			}
+		}.runTaskTimer(plugin, 5, 5);
 	}
 }
