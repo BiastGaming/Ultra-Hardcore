4180f3cf866d888ff5a6ceb2318c05e3b7c2cf97
LeonTG77 <leontg77@gmail.com>
2016-05-22 03:42:43 +0200

    Fixed xp orbs

--- src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/VeinMiner.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.Game.State;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.BlockUtils;
@@ -23,12 +24,9 @@ import com.leontg77.ultrahardcore.utils.BlockUtils;
  * @author LeonTG77
  */
 public class VeinMiner extends Scenario implements Listener {
-    private final Main plugin;
 
-    public VeinMiner(Main plugin) {
+    public VeinMiner() {
         super("VeinMiner", "When you mine a part of a ore vein while shifting the entire thing mines.");
-
-        this.plugin = plugin;
     }
 
     private static final Predicate<Material> IS_ORE = (m) -> m.name().endsWith("_ORE");
@@ -45,7 +43,7 @@ public class VeinMiner extends Scenario implements Listener {
         if (!player.isSneaking()) {
             return;
         }
-
+        
         if (!IS_ORE.test(block.getType())) {
             return;
         }
@@ -63,8 +61,16 @@ public class VeinMiner extends Scenario implements Listener {
 
                 Block thisBlock = vein.remove(0);
 
-                BlockUtils.blockBreak(null, thisBlock);
-                thisBlock.breakNaturally(player.getItemInHand());
+                BlockBreakEvent breaking = new BlockBreakEvent(thisBlock, player);
+                Bukkit.getPluginManager().callEvent(breaking);
+                
+                if (!breaking.isCancelled()) {
+                    BlockUtils.blockBreak(null, thisBlock);
+                    thisBlock.breakNaturally(player.getItemInHand());
+                }
+                
+                ExperienceOrb orb = player.getWorld().spawn(thisBlock.getLocation(), ExperienceOrb.class);
+                orb.setExperience(breaking.getExpToDrop());
             }
         }.runTaskTimer(plugin, 1, 1);
     }
