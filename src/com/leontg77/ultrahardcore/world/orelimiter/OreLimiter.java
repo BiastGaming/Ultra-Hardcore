bec9853d7d90ae2b427ad36a78acf90b9f4157ad
LeonTG77 <leontg77@gmail.com>
2016-05-22 13:12:13 +0200

    Leonified

--- src/com/leontg77/ultrahardcore/world/orelimiter/OreLimiter.java
+++ src/com/leontg77/ultrahardcore/world/orelimiter/OreLimiter.java
@@ -17,41 +17,49 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * Ore Limiter class.
+ * 
+ * @author D4mnX
+ */
 public class OreLimiter implements Listener {
-
     protected static final TObjectDoubleMap<Material> ORE_RATES;
+    
     static {
         ORE_RATES = new TObjectDoubleHashMap<>();
         ORE_RATES.put(Material.GOLD_ORE, 0.5d);
         ORE_RATES.put(Material.DIAMOND_ORE, 0.65d);
     }
 
-    protected final Random random = new Random();
-    protected final Settings settings;
+    private final Random random = new Random();
+    private final Settings settings;
 
     public OreLimiter(Settings settings) {
         this.settings = settings;
     }
 
     @EventHandler
-    protected void on(ChunkPopulateEvent event) {
+    public void on(ChunkPopulateEvent event) {
         World world = event.getWorld();
+        
         if (!settings.getWorlds().getBoolean(world.getName() + ".oreLimiter", true)) {
             return;
         }
 
-        Chunk chunk = event.getChunk();
         Set<Block> checked = Sets.newHashSet();
+        Chunk chunk = event.getChunk();
 
         for (int x = 0; x < 16; x++) {
             for (int y = 0; y < 256; y++) {
                 for (int z = 0; z < 16; z++) {
                     Block block = chunk.getBlock(x, y, z);
+                    
                     if (checked.contains(block)) {
                         continue;
                     }
 
                     Material type = block.getType();
+                    
                     if (!ORE_RATES.containsKey(type)) {
                         continue;
                     }
