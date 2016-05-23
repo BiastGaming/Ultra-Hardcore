451c3bced13c38730bbb1c225e4422eeeab6e283
LeonTG77 <leontg77@gmail.com>
2016-05-23 21:42:12 +0200

    Disabled shoot health in no name tags

--- src/com/leontg77/ultrahardcore/feature/pvp/ShootHealthFeature.java
+++ src/com/leontg77/ultrahardcore/feature/pvp/ShootHealthFeature.java
@@ -16,6 +16,7 @@ import com.leontg77.ultrahardcore.feature.Feature;
 import com.leontg77.ultrahardcore.managers.TeamManager;
 import com.leontg77.ultrahardcore.scenario.ScenarioManager;
 import com.leontg77.ultrahardcore.scenario.scenarios.Anonymous;
+import com.leontg77.ultrahardcore.scenario.scenarios.NoNameTags;
 import com.leontg77.ultrahardcore.scenario.scenarios.Paranoia;
 import com.leontg77.ultrahardcore.scenario.scenarios.SelfDiagnosis;
 import com.leontg77.ultrahardcore.scenario.scenarios.TeamHealth;
@@ -49,7 +50,7 @@ public class ShootHealthFeature extends Feature implements Listener {
         Entity attacked = event.getEntity();
         Entity attacker = event.getDamager();
 
-        if (game.isRecordedRound() || scen.getScenario(TeamHealth.class).isEnabled() || scen.getScenario(SelfDiagnosis.class).isEnabled() || scen.getScenario(Paranoia.class).isEnabled() || scen.getScenario(WTFIPTG.class).isEnabled() || scen.getScenario(Anonymous.class).isEnabled()) {
+        if (game.isRecordedRound() || scen.getScenario(NoNameTags.class).isEnabled() || scen.getScenario(TeamHealth.class).isEnabled() || scen.getScenario(SelfDiagnosis.class).isEnabled() || scen.getScenario(Paranoia.class).isEnabled() || scen.getScenario(WTFIPTG.class).isEnabled() || scen.getScenario(Anonymous.class).isEnabled()) {
             return;
         }
 
@@ -72,7 +73,7 @@ public class ShootHealthFeature extends Feature implements Listener {
                 String percent = NumberUtils.makePercent(health);
 
                 if (health > 0.0000) {
-                    killer.sendMessage(Main.PREFIX + name(player)+ " §7is now at §a" + percent + "%");
+                    killer.sendMessage(Main.PREFIX + name(player) + " §7is now at §a" + percent + "%");
                 }
             }
         }.runTaskLater(plugin, 1);
