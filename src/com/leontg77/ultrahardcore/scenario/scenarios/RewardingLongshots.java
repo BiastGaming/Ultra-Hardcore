8df4a830db9fdc636ad3544c9292a40a81851ad3
LeonTG77 <leontg77@gmail.com>
2016-06-05 01:59:46 +0200

    State, game player, prefix updates and changes

--- src/com/leontg77/ultrahardcore/scenario/scenarios/RewardingLongshots.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/RewardingLongshots.java
@@ -2,54 +2,68 @@ package com.leontg77.ultrahardcore.scenario.scenarios;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.leontg77.ultrahardcore.Game.State;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.NumberUtils;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
- * RewardingLongshots scenario class
+ * RewardingLongshots scenario class.
  * 
  * @author LeonTG77
  */
 public class RewardingLongshots extends Scenario implements Listener {
+    public static final String PREFIX = "§7[§9Rewarding Longshots§7] §f";
 
     public RewardingLongshots() {
-        super("RewardingLongshots", "When shooting and hitting people with a bow from a variable distance, you will be rewarded with various different items, the + means it gives you bows/arrows for getting longshots as well.");
+        super("RewardingLongshots", "When shooting and hitting people with a bow from a variable distance, you will be rewarded with various different items.");
     }
 
-    @Override
-    public void onDisable() {}
-
-    @Override
-    public void onEnable() {}
-
     @EventHandler(ignoreCancelled = true)
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (!(event.getDamager() instanceof Arrow) || !(event.getEntity() instanceof Player)) {
+    public void on(EntityDamageByEntityEvent event) {
+        if (!game.isState(State.INGAME)) {
+            return;
+        }
+        
+        Entity damager = event.getDamager();
+        Entity entity = event.getEntity();
+        
+        if (!(damager instanceof Arrow) || !(entity instanceof Player)) {
             return;
         }
 
-        Player player = (Player) event.getEntity();
-        Arrow damager = (Arrow) event.getDamager();
+        Player player = (Player) entity;
+        
+        if (!game.getPlayers().contains(player)) {
+            return;
+        }
+        
+        Arrow arrow = (Arrow) damager;
 
-        if (!(damager.getShooter() instanceof Player)) {
+        if (!(arrow.getShooter() instanceof Player)) {
             return;
         }
 
-        Player killer = (Player) damager.getShooter();
+        Player killer = (Player) arrow.getShooter();
+        
+        if (!game.getPlayers().contains(killer)) {
+            return;
+        }
+        
         double distance = killer.getLocation().distance(player.getLocation());
 
         if (distance < 30) {
             return;
         }
 
-        PlayerUtils.broadcast("§7[§9Rewarding Longshots§7] §f" + killer.getName() + " just got a longshot of " + NumberUtils.formatDouble(distance) + " meters!");
+        PlayerUtils.broadcast(PREFIX + killer.getName() + " just got a longshot of " + NumberUtils.formatDouble(distance) + " blocks!");
 
         if (distance <= 49) {
             PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT));
